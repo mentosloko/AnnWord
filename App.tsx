bf1923e4f15c508eb16a7224791d796870cd9328
mentosloko <mentosloko@gmail.com>
2026-04-07 13:42:13 +0300

    restore: App.tsx to previous working version (full file)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,1 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { GameSettings, GameState, CharStatus, UserProfile, WordLength, EnrichedWord, ViewState, DictionarySource, DifficultyLevel, PetState, UserStats } from './types';
-import { MAX_GUESSES } from './constants';
-import { COMMON_WORDS_EN, ALL_WORDS_EN } from './dictionaries/english';
-import { Grid } from './components/Grid';
-import { Keyboard } from './components/Keyboard';
+PLACEHOLDER
