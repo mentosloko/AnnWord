78d99dc52bd0e9064f58e941c694239c6981646e
mentosloko <mentosloko@gmail.com>
2026-06-07 23:18:38 +0300

    Restore compilable profile screen

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
 import { UserProfile } from '../../types';
 import { ScreenContainer } from '../layout/ScreenContainer';
-
-interface ProfileScreenProps {
-  userProfile: UserProfile;
-  isAuthenticated: boolean;
-  onBackHome: () => void;
-  onOpenShop: () => void;
-  onOpenPetRoom: () => void;
-  onLogin: () => void;
-}
-
-export const ProfileScreen: React.FC<ProfileScreenProps> = ({ userProfile, isAuthenticated, onBackHome, onOpenShop, onOpenPetRoom, onLogin }) => {
-  const isKids = userProfile.role === 'parent' || userProfile.accountMode === 'parent
+interface ProfileScreenProps{userProfile:UserProfile;isAuthenticated:boolean;onBackHome:()=>void;onOpenShop:()=>void;onOpenPetRoom:()=>void;onLogin:()=>void}
+export const ProfileScreen:React.FC<ProfileScreenProps>=({userProfile,isAuthenticated,onBackHome,onOpenShop,onOpenPetRoom,on
