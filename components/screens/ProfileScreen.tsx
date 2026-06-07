d2988a5d18d836a5726de8472f9132e4a05aa626
mentosloko <mentosloko@gmail.com>
2026-06-07 22:20:43 +0300

    Restore product-aware profile screen

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { UserProfile } from '../../types';
-import { getCharacterProgressPercent, getCharacterProgressText, getCharacterStageLabel, normalizeMoodScore } from '../../services/gamificationRules';
-import { getPetEmoji, getPetNeedSnapshot } from '../../services/petEngine';
+import { getPetEmoji } from '../../services/petEngine';
 import { getPetCharacterAssetUrl } from '../../services/petAssets';
 import { ScreenContainer } from '../layout/ScreenContainer';
-import { Coin
+
+interface ProfileScreenProps {
+  userProfile: UserProfile;
+  isAuthenticated: boolean;
+  onBackHome: () => void;
+  onOpenShop: () => void;
+  onOpenPetRoom: () => void;
+ 
