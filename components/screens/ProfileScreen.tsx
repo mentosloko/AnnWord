163811e95425591677055d4ac38a7d8c78ee9571
mentosloko <mentosloko@gmail.com>
2026-06-07 23:27:12 +0300

    Restore valid profile screen

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import { UserProfile } from '../../types';
 import { ScreenContainer } from '../layout/ScreenContainer';
-interface ProfileScreenProps{userProfile:UserProfile;isAuthenticated:boolean;onBackHome:()=>void;onOpenShop:()=>void;onOpenPetRoom:()=>void;onLogin:()=>void}
-export const ProfileScreen:React.FC<ProfileScreenProps>=({userProfile,isAuthenticated,onBackHome,onOpenShop,onOpenPetRoom,on
+interface P{userProfile:UserProfile;isAuthenticated:boolean;onBackHome:()=>void;onOpenShop:()=>void;onOpenPetRoom:()=>void;onLogin:()=>void}
+export const ProfileScreen:React.FC<P>=({userProfile,isAuthenticated,onBackHome,onOpenShop,onOpenPetRoom,onLogin})=>{const kids=user
