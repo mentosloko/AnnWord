e2dff6f5e13537b694bd5d066668a5c57af895b2
mentosloko <mentosloko@gmail.com>
2026-06-07 23:37:56 +0300

    Restore minimal valid profile component

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import { UserProfile } from '../../types';
-import { ScreenContainer } from '../layout/ScreenContainer';
 interface P{userProfile:UserProfile;isAuthenticated:boolean;onBackHome:()=>void;onOpenShop:()=>void;onOpenPetRoom:()=>void;onLogin:()=>void}
-export const ProfileScreen:React.FC<P>=({userProfile,isAuthenticated,onBackHome,onOpenShop,onOpenPetRoom,onLogin})=>{const kids=user
+export const ProfileScreen:React.FC<P>=p=><div><button onClick={p.onBackHome}>Back</button><div>{p.userProfile.username}</div></div>;
