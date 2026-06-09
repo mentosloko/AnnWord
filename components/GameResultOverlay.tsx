c088b9f565ce2b7cd39dfc51bc7ecbfe788191e8
mentosloko <mentosloko@gmail.com>
2026-06-09 19:32:46 +0300

    Restore valid minimal result overlay

--- components/GameResultOverlay.tsx
+++ components/GameResultOverlay.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 export const GameResultOverlay=(p:any)=>{
  if(!p.isOpen)return null;
- return React.createElement('div',null,
-  React.createElement('h2',null,p.title),
-  p.subtitle&&React.createElement('p',null,p.subtitle),
-  p.details&&React.createElement('div',null,p.details),
-  React.createElement('button',{onClick:p.onPrimary},p.primaryLabel||'Again'),
-  p.onSecondary&&React.createElement('button',{onClick
+ const title=React.createElement('h2',null,p.title);
+ const sub=p.subtitle?React.createElement('p',null,p.subtitle):null;
+ const det=p.details?React.createElement('div',null,p.details):null;
+ return React.createElement('div',null,title,sub,det);
+};
