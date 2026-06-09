32607d61594ee3f8abf5120ec5246b144cac690c
mentosloko <mentosloko@gmail.com>
2026-06-09 19:38:49 +0300

    Fix minimal result overlay

--- components/GameResultOverlay.tsx
+++ components/GameResultOverlay.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-export const GameResultOverlay=(p:any)=>{
- if(!p.isOpen)return null;
- const a={onPointerUp:p.onPrimary};
- const b={onPointerUp:p.onSecondary};
- return React.createElement('div',null,
-  React.createElement('h2',null,p.title),
-  p.subtitle&&React.createElement('p',null,p.subtitle),
-  p.details&&React.createElement('div',null,p.details),
-  React.createElement('button',a,p.primaryLabel||'Again'),
-  p.onSecondary&&React.create
+import React from'react';export const GameResultOverlay=(p:any)=>p.isOpen?React.createElement('div',null,p.title,p.details,React.createElement('button',{onPointerUp:p.onPrimary},p.primaryLabel||'Again'),p.onSecondary&&React.createElement('button',{onPointerUp:p.onSecondary},p.secondaryLabel||'Menu')):null;
