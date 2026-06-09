306fc6b9a01cb408d9d783e12c3c8a90426e3df6
mentosloko <mentosloko@gmail.com>
2026-06-09 11:54:10 +0300

    Restore minimal valid FamilySetupScreen

--- components/screens/FamilySetupScreen.tsx
+++ components/screens/FamilySetupScreen.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { ChildSetupResult } from '../../services/familyAccountService';
+import React from 'react';
 import { ScreenContainer } from '../layout/ScreenContainer';
 
-interface FamilySetupScreenProps {
-  onCreateChild: (childName: string, parentPin: string) => Promise<ChildSetupResult>;
-  onComplete: (result: ChildSetupResult) => void;
-  onBackHome: () => void;
-}
-
-export const FamilySetupScreen: React.FC<FamilySetupScreenProps> = ({ onCreateChild,
+export const FamilySetupScreen: React.FC<any> = ({ onBackHome }) => {
+  return (
+    <ScreenContainer className="max-w-lg pb-20">
+      <button type="button" onClick={onBackHome}>Back</button>
+      <section>
+        <h1>Setup</h1>
+        <p>V1 account setup screen.</p>
+      </section>
+    </ScreenContainer>
+  );
+};
