08c514cb66fe6045f8f7e6d7d90d58eb2ee2a813
mentosloko <mentosloko@gmail.com>
2026-06-23 12:20:09 +0300

    Accept urlencoded API request bodies

--- server/yandex-api.ts
+++ server/yandex-api.ts
@@ -15,6 +15,7 @@ const app = express();
 
 app.disable("x-powered-by");
 app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(
   cors({
     origin(origin, callback) {
