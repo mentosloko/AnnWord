c1c3406ccc08b3568b24e8cd325b78ec6381521b
mentosloko <mentosloko@gmail.com>
2026-07-27 21:25:50 +0300

    Complete Spotlight grade 9 data

---
+++ dictionaries/premium/spotlight/spotlight_grade_9.data.ts
@@ -0,0 +1,8 @@
+import part1 from './spotlight_grade_9.part1';
+import part2 from './spotlight_grade_9.part2';
+import part3 from './spotlight_grade_9.part3';
+import part4 from './spotlight_grade_9.part4';
+import part5 from './spotlight_grade_9.part5';
+import part6 from './spotlight_grade_9.part6';
+
+export default part1 + part2 + part3 + part4 + part5 + part6;
