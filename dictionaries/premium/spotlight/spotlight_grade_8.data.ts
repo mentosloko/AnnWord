e29e1c662e3e77724ba6d0558d39d2ad95d09482
mentosloko <mentosloko@gmail.com>
2026-07-27 21:09:08 +0300

    Assemble Spotlight grade 8 data

---
+++ dictionaries/premium/spotlight/spotlight_grade_8.data.ts
@@ -0,0 +1,8 @@
+import part1 from './spotlight_grade_8.part1';
+import part2 from './spotlight_grade_8.part2';
+import part3 from './spotlight_grade_8.part3';
+import part4 from './spotlight_grade_8.part4';
+import part5 from './spotlight_grade_8.part5';
+import part6 from './spotlight_grade_8.part6';
+
+export default part1 + part2 + part3 + part4 + part5 + part6;
