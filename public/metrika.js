8439d77c18ffd211297f35660eea82fc2c791e92
mentosloko <mentosloko@gmail.com>
2026-09-01 11:06:28 +0300

    Defer Metrika network load until first render completes

--- public/metrika.js
+++ public/metrika.js
@@ -1,19 +1,31 @@
 (function () {
   var counterId = 112133624;
+  var tagUrl = 'https://mc.yandex.ru/metrika/tag.js?id=' + counterId;
   window.__ANNWORD_METRIKA_ID__ = counterId;
 
-  (function (m, e, t, r, i, k, a) {
-    m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments); };
-    m[i].l = 1 * new Date();
+  window.ym = window.ym || function () { (window.ym.a = window.ym.a || []).push(arguments); };
+  window.ym.l = 1 * new Date();
+
+  function loadTag() {
     for (var j = 0; j < document.scripts.length; j += 1) {
-      if (document.scripts[j].src === r) return;
+      if (document.scripts[j].src === tagUrl) return;
     }
-    k = e.createElement(t);
-    a = e.getElementsByTagName(t)[0];
-    k.async = 1;
-    k.src = r;
-    a.parentNode.insertBefore(k, a);
-  })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=' + counterId, 'ym');
+    var script = document.createElement('script');
+    var firstScript = document.getElementsByTagName('script')[0];
+    script.async = 1;
+    script.src = tagUrl;
+    firstScript.parentNode.insertBefore(script, firstScript);
+  }
+
+  function loadTagAfterFirstRender() {
+    window.setTimeout(loadTag, 0);
+  }
+
+  if (document.readyState === 'complete') {
+    loadTagAfterFirstRender();
+  } else {
+    window.addEventListener('load', loadTagAfterFirstRender, { once: true });
+  }
 
   window.ym(counterId, 'init', {
     defer: true,
